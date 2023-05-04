b8f14dc3b2fc7f0e9d742e464f585dfe0f639569
amica073 <119950068+ayushguptacse14@users.noreply.github.com>
2023-05-04 20:50:14 +0530

    Update 02_deploy_Rollup.ts

--- contracts/deploy/02_deploy_Rollup.ts
+++ contracts/deploy/02_deploy_Rollup.ts
@@ -22,7 +22,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   try {
     const { stdout: pwdOutput } = await execPromise("pwd && ls -la");
     console.log("initial VM hash path is:",pwdOutput);
-    const { stdout: pwdOutput1 } = await execPromise("cd ../clients && pwd && ls -la");
+    const { stdout: pwdOutput1 } = await execPromise("cd ../clients/geth/specular/sbin && pwd && ls -la");
     console.log("initial VM hash path 1 is:",pwdOutput1);
 
     const { stdout } = await execPromise(
